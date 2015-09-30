c352b63a61e92aaba4e1ba1fa3d741582a71c4b1
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2015-09-30 17:26:24 -0500

    fixed str/repr, bug fixes

--- Geometry/point.py
+++ Geometry/point.py
@@ -12,52 +12,116 @@ from .exceptions import *
 
 class Point(object):
     '''
-    A three dimensional Point initialized with x,y,z values supplied
-    or the origin if no ordinates are given.
+    A three dimensional Point initialized with x,y,z values
+    supplied or the origin if no ordinates are given.
 
-    You can initialize this thing practically any way you want:
-        	a = Point()
-                b = Point(None)
-        	c = Point(x)
-        	d = Point(x,y)
-        	e = Point(x,y,z)
-                f = Point([])
-        	g = Point([x])
-        	h = Point([x,y])
-        	i = Point([x,y,z])
-                j = Point(())
-                k = Point((x))
-                l = Point((x,y))
-                m = Point((x,y,z))
-                n = Point({})
-                o = Point({'x':x,'y':y,'z':z})
-        	p = Point(self.__class__(...))
-        	q = Point(x=x,y=y,z=z)
-                r = Point(<any object that defines attributes x,y,z>)
-
-    Calls resulting in Origin:
-                s = Point()
-                t = Point(None)
-                u = Point([])
-                v = Point(())
-                w = Point({})
-                x = Point(<any object whose attributes x,y,z are zero>)
-
-    Note: Subclasses should always return an object initialized with
-          origin coordinates if called with no arguments.  If not, things
-          will break.
-    '''
-
+    But wait! There's more.  This hashable point is designed to be
+    flexible and easy to use with a comprehensive set of operators and
+    functions:
     
+    Operations
+    ----------
+
+     +,  -,  *,  /,  //,  %, ** +=, -=, *=, /=, //=, %=, **==, ==, !=
+
+    Operands can be other Point subclasses or scalar numbers.
+    
+    Methods
+    -------
+
+                ccw: counter-clockwise function, also know as:
+                     "The beating red heart of computational geometry."
+
+              cross: cross product of two points.
+           distance: euclidean distance to another point.
+    distanceSquared: euclidean distance squared, useful for ordering points.
+                dot: dot product of two points.
+          isBetween: xyz bounds checking a point between two other points.
+                     see also: isBetweenX, is BetweenY, isBetweenZ
+              isCCW: is this angle a counter-clockwise rotation?
+        isCollinear: are these three points on a line?
+           midpoint: point between two points on the line between them.
+
+    Class Methods
+    -------------
+
+                     gaussian: Random point with gaussian distribution
+               randomLocation: Random point within a circular bound
+    randomLocationInRectangle: Random point within a rectangular bound
+    
+
+    Random Usage Examples
+    ---------------------
+
+    >>> from Geometry import Point
+    >>> a = Point(x)
+    >>> b = Point(x,y)
+    >>> c = Point(x,y,z)
+    >>> d = Point([x])
+    >>> e = Point([x,y])
+    >>> f = Point([x,y,z])
+    >>> g = Point((x))
+    >>> h = Point((x,y))
+    >>> i = Point((x,y,z))
+    >>> j = Point({'x':x,'y':y,'z':z})
+    >>> k = Point(Point(...))
+    >>> l = Point(x=x,y=y,z=z)
+    >>> m = Point(<any object that defines attributes x,y,z>)
+    >>> Origins = [Point(),Point(None),Point([0]*3),
+    ...            Point([]),Point(()),Point({})]
+
+    Points are just buckets to hold x, y and z values right?
+
+    Get them any way you want them!
+
+    >>> p = Point(range(0,3))
+    >>> p.x, p.y, p.z, p.w
+    (0.0, 1.0, 2.0, 1.0)
+    >>> p.xyz
+    [0.0, 1.0, 2.0]
+    >>> p.xy, p.yz, p.xz
+    ([0.0, 1.0], [1.0, 2.0], [0.0, 2.0])
+    >>> p.xyzw
+    [0.0, 1.0, 2.0, 1.0]
+
+    Also, assignment:
+
+    >>> p.xyz = [4,5,6]
+    >>> p.xyz = [11] * 2
+    >>> p
+    [11.0, 11.0, 6.0]
+
+    If fact, any construct you can use to initialize a Point object can
+    be used to set any of the coordinate attributes.
+
+    Want to find the average point for a group of 1000 random points?
+
+    >>> manyPoints = [Point.gaussian() for x in range(0,1000)]
+    >>> center = sum(manyPoints) / len(manyPoints)
+    >>> type(center)
+    <class 'Geometry.point.Point'>
+
+    Go forth and geometer!
+
+    Subclassing Notes:
+
+    0 Initialization:
+
+      Subclasses should always return an object initialized with
+      origin coordinates if called with no arguments.  If not, things
+      will break.
+
+    '''
     ordinateNamesAll = 'xyzw'
-    ordinateNamesXYZ = 'xyz'
-    ordinateNamesXY = 'xy'
-    ordinateNamesYZ = 'yz'
-    ordinateNamesXZ = 'xz'
-    ordinateNameX = 'x'
-    ordinateNameY = 'y'
-    ordinateNameZ = 'z'
-    ordinateNameW = 'w'
+    ordinateNamesXYZ = ordinateNamesAll[:3]
+    ordinateNameX = ordinateNamesAll[0]
+    ordinateNameY = ordinateNamesAll[1]
+    ordinateNameZ = ordinateNamesAll[2]
+    ordinateNameW = ordinateNamesAll[3]
+    ordinateNamesXY = ordinateNameX+ordinateNameY
+    ordinateNamesYZ = ordinateNameY+ordinateNameZ
+    ordinateNamesXZ = ordinateNameX+ordinateNameZ
+
 
     @classmethod
     def gaussian(cls,mu=0,sigma=1):
@@ -76,7 +140,8 @@ class Point(object):
         
     @classmethod
     def randomLocationInRectangle(cls,origin=None,width=1,height=1):
-        ''':param: origin - optional Point subclass 
+        '''
+        :param: origin - optional Point subclass or Point initializer
         :param: width  - optional integer
         :param: height - optional integer
         :return: Point subclass
@@ -85,51 +150,48 @@ class Point(object):
         by the rectangle defined by (origin,width,height).  
 
         If a rectangle is not supplied, a unit square with vertex A
-        on the origin is used.
+        on the origin is used by default.
 
         '''
-        if origin is None:
-            origin = cls()
+        
+        origin = cls(origin)   # handles conversion of point initializers
 
-        O = cls()
-        O += origin             # this will typically be a no-op
-
-        x = random.randint(O.x,O.x+width)
-        y = random.randint(O.y,O.y+height)
+        x = random.randint(O.x,origin.x+width)
+        y = random.randint(O.y,origin.y+height)
             
         return cls(x,y,O.z)
     
     @classmethod
     def randomLocation(cls,origin=None,radius=1):
-        ''':param: radius - float
-        :param: origin - optional Point subclass
+        '''
+        :param: origin - optional Point subclass or Point initializer
+        :param: radius - float
         :return: Point sublcass
 
         Returns a Point with random x,y coordinates bounded by the
-        circle defined by (origin,radius). 
+        circle defined by (origin,radius).
 
-        If a circle is given, a unit circle at the origin is used.
-        
-        If origin is not supplied, (0,0) is assumed.
-
+        If a circle is not supplied, a unit circle at the origin is used
+        by default.
         '''
         
-        if origin is None:
-            origin = cls()
-
-        O = cls()
-        O += origin
-
+        origin = cls(origin)
         
         t = 2.0 * math.pi * random.random()
         r = random.random() * random.random()
         if r > 1:
             r = 2 - r
             
-        x = int(radius * r * math.cos(t) + O.x)
-        y = int(radius * r * math.sin(t) + O.y)
+        x = int(radius * r * math.cos(t) + origin.x)
+        y = int(radius * r * math.sin(t) + origin.y)
         
         return cls(x,y)
+    
+    @classmethod
+    def units(cls):
+        '''
+        '''
+        return [cls(1,0,0),cls(0,1,0),cls(0,0,1)]
     
     def __init__(self,*args,**kwds):
 
@@ -139,8 +201,6 @@ class Point(object):
             return
 
         if nargs == 1:
-            if args[0] is None:
-                return
             self.xyz = args[0]
             
         if nargs > 1:
@@ -156,21 +216,19 @@ class Point(object):
         try:
             return self._x
         except AttributeError:
-            self._x = 0.
+            self._x = float(0)
         return self._x
 
     @x.setter
     def x(self,newValue):
-
-        if newValue is None:
-            self._x = 0
-            return
-
+        
         try:
             self._x = float(newValue)
             return
         except TypeError:
-            pass
+            if newValue is None:
+                self._x = float(0)
+                return
         
         try:
             self._x = float(newValue[0])
@@ -202,7 +260,7 @@ class Point(object):
         try:
             return self._y
         except AttributeError:
-            self._y = 0.
+            self._y = float(0)
         return self._y
 
     @y.setter
@@ -212,8 +270,9 @@ class Point(object):
             self._y = float(newValue)
             return
         except TypeError:
-            pass
-
+            if newValue is None:
+                self._y = float(0)
+                return 
         try:
             self._y = float(newValue[0])
             return
@@ -236,7 +295,6 @@ class Point(object):
         raise UngrokkableObject(newValue)
 
 
-
     @property
     def z(self):
         '''
@@ -245,7 +303,7 @@ class Point(object):
         try:
             return self._z
         except AttributeError:
-            self._z = 0.0
+            self._z = float(0)
         return self._z
 
     @z.setter
@@ -255,7 +313,9 @@ class Point(object):
             self._z = float(newValue)
             return
         except TypeError:
-            pass
+            if newValue is None:
+                self._z = float(0)
+                return
 
         try:
             self._z = float(newValue[0])
@@ -283,7 +343,7 @@ class Point(object):
     @property
     def w(self):
         '''
-        W parameter to make square matrices.
+        W parameter to make square matrices, float.
         '''
         return 1.0
 
@@ -291,33 +351,28 @@ class Point(object):
     def xyz(self):
         '''
         Returns an array [x,y,z].
+
+        Set the values of x, y and z with obj, where obj can be:
+        1. an iterable with 1, 2 or 3 number items
+        2. an object defining x, y, or z attributes.
+        3. a dictionary with x, y, or z keys.
+        4. a scalar float, only sets x.
+        5. None, sets all coordinates to zero.
         '''
         return [self.x,self.y,self.z]
 
     @xyz.setter
     def xyz(self,iterable):
-        '''
-        Set the values of x, y and z with obj, where obj can be:
-        1. an iterable with 1, 2 or 3 items
-        2. an object defining x, y, or z attributes 
-        3. a dictionary with x, y, or z keys
-        4. a scalar float, only sets x
-
-        '''
-
+        
         if self._complex_setter(iterable,self.__class__.ordinateNamesXYZ):
             return
-        
-        # straight iteration, hope for three items
+
         try:
             self.x,self.y,self.z = iterable[:3]
             return
         except (TypeError,ValueError):
-            # not iterable
-            # unpack error, not enough values to unpack
             pass
         
-        # hand off to xy.setter
         self.xy = iterable
 
 
@@ -325,14 +380,13 @@ class Point(object):
     def xyzw(self):
         '''
         Returns an array of [x,y,z,1]
+        Follows xyz setter rules for the values of x, y and z.
+        Values for w are ignored.
         '''
         return [self.x,self.y,self.z,self.w]
     
     @xyzw.setter
     def xyzw(self,iterable):
-        '''
-        Set the values of x, y and z. Values for w are ignored.
-        '''
         self.xyz = iterable
 
     @property
@@ -344,15 +398,12 @@ class Point(object):
 
     @xy.setter
     def xy(self,iterable):
-        '''
-        Set the values of x and z with an iterable.
-        '''
-
+        
         if self._complex_setter(iterable,self.__class__.ordinateNamesXY):
             return
 
         try:
-            self.x,self.y = iterable
+            self.x,self.y = iterable[:2]
             return
         except (ValueError,TypeError):
             pass
@@ -368,15 +419,12 @@ class Point(object):
 
     @yz.setter
     def yz(self,iterable):
-        '''
-        Set the values of y and z with an iterable.
-        '''
-
+        
         if self._complex_setter(iterable,self.__class__.ordinateNamesYZ):
             return
 
         try:
-            self.y,self.z = iterable
+            self.y,self.z = iterable[:2]
             return
         except (ValueError,TypeError):
             pass
@@ -392,15 +440,12 @@ class Point(object):
 
     @xz.setter
     def xz(self,iterable):
-        '''
-        Set the values of x and z with an iterable.
-        '''
 
         if self._complex_setter(iterable,self.__class__.ordinateNamesXZ):
             return
         
         try:
-            self.x,self.z = iterable
+            self.x,self.z = iterable[:2]
             return
         except (ValueError,TypeError):
             pass
@@ -410,22 +455,33 @@ class Point(object):
     @property
     def bytes(self):
         '''
-        A bytes buffer of repr(self) encoded with utf-8.
+        A buffer encoded with utf-8 of the string 'repr(self)', bytes.
         '''
         return bytes(repr(self),'utf-8')
+
+    @property
+    def mapping(self):
+        '''
+        The Point subclass encoded as a dict, dict.
+        '''
+        return { self.ordinateNameX:self.x,
+                 self.ordinateNameY:self.y,
+                 self.ordinateNameZ:self.z,
+                 self.ordinateNameW:self.w }
     
-
     def __str__(self):
-        return '(%s)' %(','.join([str(v) for v in self.xyz]))
-
+        '''
+        '''
+        return 'x={x},y={y},z={z}'.format(**self.mapping)
+    
     def __repr__(self):
         '''
         :return: string
 
         String representation of the current instance.
         '''
-        return '%s%s' % (self.__class__.__name__,str(self))
-
+        return '{klass}({args})'.format(klass=self.__class__.__name__,
+                                        args=str(self))
 
     def __hash__(self):
         '''
@@ -644,7 +700,7 @@ class Point(object):
                 return self.__rfloordiv__(other)
         except ZeroDivisionError:
             pass
-        raise ZeroDivisionError('zero present in %s' %(iterable))
+        raise ZeroDivisionError('zero present in %s' %(other))
 
     def __rfloordiv__(self,other):
         '''
@@ -824,8 +880,7 @@ class Point(object):
             return getattr(self,self.__class__.ordinateNamesAll[key])
         except IndexError:
             pass
-        
-        raise IndexError( 'index %d out of range' % (key))
+        raise IndexError( 'index %s out of range' % (key))
 
     def __setitem__(self,key,value):
         '''
@@ -833,7 +888,11 @@ class Point(object):
 
         Attempts to set 'w' are ignored without raising an error.
         '''
-        setattr(self,self.__class__.ordinateNamesAll[key],value)
+        try:
+            setattr(self,self.__class__.ordinateNamesAll[key],value)
+        except IndexError:
+            pass
+        raise IndexError('index %s out of range' % (key))
 
 
     def _complex_setter(self,obj,keys):
@@ -849,6 +908,11 @@ class Point(object):
         Returns True if at least zero or more values is found without
         incurring an exception.
         '''
+
+        if obj is None:
+            for key in keys:
+                setattr(self,key,0.0)
+            return True
 
         try:
             if len(obj) == 0:
@@ -904,13 +968,8 @@ class Point(object):
         Returns s1 + s2 + s3
         '''
 
-        if self.z and other.z:
-            s0 = (self.y*other.z) - (self.z*other.y)
-            s1 = (self.z*other.x) - (self.x*other.z)
-        else:
-            s0 = 0
-            s1 = 0
-    
+        s0 = (self.y*other.z) - (self.z*other.y)
+        s1 = (self.z*other.x) - (self.x*other.z)
         s2 = (self.x*other.y) - (self.y*other.x)
 
         return s0 + s1 + s2
@@ -920,7 +979,7 @@ class Point(object):
         :param: other - Point subclass
         :return: Point subclass
         
-        Point midway between 'a' and 'b'.
+        Point midway between 'self' and 'other'.
 
         '''
         m = self + other  # create a new point
@@ -1037,7 +1096,7 @@ class Point(object):
         :return: boolean
 
         True if the angle formed by self,b,c has a counter-clockwise
-        rotation.
+        rotation around the specified axis ( 'z' by default ).
 
         Can raise CollinearPoints() if self,b,c all lie on the same
         line.
@@ -1060,7 +1119,7 @@ class Point(object):
 
         Returns an integer signifying whether the angle described by
         the points self,b,c has a counter-clockwise rotation around
-        the specified axis.
+        the specified axis ( 'z' by default).
         
         > 0 : counter-clockwise
           0 : points are collinear 
@@ -1080,7 +1139,7 @@ class Point(object):
         if axis == 'x':
             return (bsuba.y * csuba.z) - (bsuba.z * csuba.y)
 
-        raise ValueError("axis '%s' no recognized." % (axis))
+        raise ValueError("axis '%s' not recognized." % (axis))
                 
 
 
