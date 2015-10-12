90bf3e2713af6a1c086f34e9a8127726a7abfb35
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2015-10-12 13:41:58 -0500

    initial commit

---
+++ tests/test_circle.py
@@ -0,0 +1,27 @@
+
+import unittest
+import sys
+sys.path.append('..')
+
+from Geometry import Point, Circle
+from Geometry.exceptions import *
+
+class CircleInitializationTestCase(unittest.TestCase):
+    pass
+
+class CircleAttributeSettersTestCase(unittest.TestCase):
+    pass
+
+class CircleAttributeGettersTestCase(unittest.TestCase):
+    pass
+
+class CircleClassmethodsTestCase(unittest.TestCase):
+    pass
+
+class CircleInstanceMethodsTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
