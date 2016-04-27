db63eddd57195e669a9c46787a35f4e3fe8655e3
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2016-04-27 10:14:40 -0500

    reworked to use new Point

--- Geometry/triangle.py
+++ Geometry/triangle.py
@@ -2,7 +2,7 @@
 '''
 
 import math
-from .point import Point
+from .point2 import Point
 from .line import Segment
 from .exceptions import *
 
@@ -71,6 +71,10 @@ class Triangle(object):
 
     def __init__(self, *args, **kwds):
 
+        self(*args,**kwds)
+
+    def __call__(self, *args, **kwds):
+
         if len(args) == 0 and len(kwds) == 0:
             return
 
@@ -96,7 +100,7 @@ class Triangle(object):
             pass
         self._A = Point()
         return self._A
-
+    
     @A.setter
     def A(self, newValue):
         self.A.xyz = newValue
@@ -110,9 +114,8 @@ class Triangle(object):
             return self._B
         except AttributeError:
             pass
-        self._B = Point(1, 0)
+        self._B = Point(1,0)
         return self._B
-
     @B.setter
     def B(self, newValue):
         self.B.xyz = newValue
@@ -126,19 +129,24 @@ class Triangle(object):
             return self._C
         except AttributeError:
             pass
-        self._C = Point(0, 1)
+        self._C = Point(0,1)
         return self._C
-
+    
     @C.setter
     def C(self, newValue):
-        self.C.xyz = newValue
+        self.C.xyz = newValue        
 
     @property
     def ABC(self):
         '''
         A list of the triangle's vertices, list.
         '''
-        return [self.A, self.B, self.C]
+        try:
+            return self._ABC
+        except AttributeError:
+            pass
+        self._ABC = [self.A, self.B, self.C]
+        return self._ABC
 
     @ABC.setter
     def ABC(self, iterable):
@@ -147,23 +155,50 @@ class Triangle(object):
     @property
     def AB(self):
         '''
-        Line segment with endpoints A and B, Segment subclass.
+        List of vertices A and B, list.
         '''
-        return Segment(self.A, self.B)
+        try:
+            return self._AB
+        except AttributeError:
+            pass
+        self._AB = [self.A, self.B]
+        return self._AB
+
+    @AB.setter
+    def AB(self, iterable):
+        self.A, self.B = iterable
 
     @property
     def AC(self):
         '''
-        Line segment with endpoints A and C, Segment subclass.
+        List of verticies A and C, list.
         '''
-        return Segment(self.A, self.C)
+        try:
+            return self._AC
+        except AttributeError:
+            pass
+        self._AC = [self.A, self.C]
+        return self._AC
+
+    @AC.setter
+    def AC(self, iterable):
+        self.A, self.C = iterable
 
     @property
     def BC(self):
         '''
         Line segment with endpoints B and C, Segment subclass.
         '''
-        return Segment(self.B, self.C)
+        try:
+            return self._BC
+        except AttributeError:
+            pass
+        self._BC = [self.B, self.C]
+        return self._BC
+        
+    @BC.setter
+    def BC(self, iterable):
+        self.B, self.C = iterable
 
     @property
     def vertices(self):
@@ -175,10 +210,17 @@ class Triangle(object):
     @property
     def sides(self):
         '''
-        List of line segments, list.
+        Dictionary of line segments, dict.
 
         '''
-        return [self.AB, self.BC, self.AC]
+        try:
+            return self._sides
+        except AttributeError:
+            pass
+
+        self._sides = { 'AB':self.AB, 'BC':self.BC, 'AC':self.AC }
+        
+        return self._sides
 
     @property
     def hypotenuse(self):
@@ -186,59 +228,38 @@ class Triangle(object):
         The longest side of the triangle.
 
         '''
-        s = self.sides
-        s.sort(key=lambda x: x.length)
-        return s[-1]
+        return max(self.ab, self.bc, self.ac)
 
     @property
-    def alphaRadians(self):
+    def alpha(self):
         '''
         The angle described by CAB in radians, float.
 
         '''
-        return self.AB.radiansBetween(self.AC)
+        return Segment(self.C,self.A).radiansBetween(Segment(self.A,self.B))
 
     @property
-    def alphaDegrees(self):
-        return math.degrees(self.alphaRadians)
-
-    @property
-    def betaRadians(self):
+    def beta(self):
         '''
         The angle described by ABC in radians, float.
 
         '''
-        BA = Segment(self.B, self.A)
-        return BA.radiansBetween(self.BC)
-
+        return Segment(self.A,self.B).radiansBetween(Segment(self.B,self.C))
+        
     @property
-    def betaDegrees(self):
-        return math.degrees(self.betaRadians)
-
-    @property
-    def gammaRadians(self):
+    def gamma(self):
         '''
         The angle described by BCA in radians, float.
 
         '''
-        CB = Segment(self.C, self.B)
-        return CB.radiansBetween(Segment(self.C, self.A))
+        
+        return Segment(self.B, self.C).radiansBetween(Segment(self.C, self.A))
 
     @property
-    def gammaDegrees(self):
+    def angles(self):
         '''
         '''
-        return math.degrees(self.gammaRadians)
-
-    @property
-    def anglesRadians(self):
-        '''
-        '''
-        return [self.alphaRadians, self.betaRadians, self.gammaRadians]
-
-    @property
-    def anglesDegrees(self):
-        return [self.alphaDegrees, self.betaDegrees, self.gammaDegrees]
+        return [self.alpha, self.beta, self.gamma]
 
     @property
     def ab(self):
@@ -291,10 +312,10 @@ class Triangle(object):
     @property
     def semiperimeter(self):
         '''
-        semiperimeter = (|AB|+|AC|+|BC|) / 2
+        semiperimeter = (|AB|+|BC|+|AC|) / 2
 
         '''
-        return sum([x.length for x in self.sides]) / 2.
+        return sum([self.ab, self.bc, self.ac])/2
 
     @property
     def isEquilateral(self):
@@ -338,7 +359,7 @@ class Triangle(object):
         rad) angle.
         '''
         half_pi = math.pi / 2
-        for a in self.anglesRadians:
+        for a in self.angles:
             if a == half_pi:    # epsilon check?
                 return True
         return False
@@ -352,7 +373,7 @@ class Triangle(object):
         '''
         half_pi = math.pi / 2
 
-        for a in self.anglesRadians:
+        for a in self.angles:
             if a > half_pi:    # epsilon check?
                 return True
         return False
@@ -364,26 +385,16 @@ class Triangle(object):
         '''
 
         half_pi = math.pi / 2
-        for a in self.anglesRadians:
+        for a in self.angles:
             if a >= half_pi:    # epsilon check?
                 return False
         return True
 
-    @property
-    def mapping(self):
-        '''
-        Mapping of vertex names to vertices, dict.
-        '''
-        return {self.vertexNameA: self.A,
-                self.vertexNameB: self.B,
-                self.vertexNameC: self.C}
-
     def __str__(self):
-        return 'A={A}, B={B}, C={C}'.format(**self.mapping)
+        return 'A=({t.A!s}), B=({t.B!s}), C=({t.C!s})'.format(t=self)
 
     def __repr__(self):
-        return '{klass}({args})'.format(klass=self.__class__.__name__,
-                                        args=str(self))
+        return '{o.__class__.__name__}({o!s})'.format(o=self)
 
     def __eq__(self, other):
         '''
@@ -416,7 +427,7 @@ class Triangle(object):
 
     def altitude(self, side='AB'):
         '''
-        :param: side - optional string
+        :side: optional string
         :return: float
 
         The shortest distance from the specified side to the opposite point.
@@ -439,7 +450,7 @@ class Triangle(object):
     def flip(self, side='AB'):
         '''
         :param: side - optional string
-        :return: None
+        :return: self
 
         The optional side paramater should have one of three values:
         AB, BC, or AC.
@@ -454,19 +465,19 @@ class Triangle(object):
             tmp = Point(self.A)
             self.A = self.B
             self.B = tmp
-            return
+            return self
 
         if side == 'BC':
             tmp = Point(self.B)
             self.B = self.C
             self.C = tmp
-            return
+            return self
 
         if side == 'AC':
             tmp = Point(self.A)
             self.A = self.C
             self.C = tmp
-            return
+            return self
 
         msg = "expecting 'AB', 'BC' or 'AC', got '{side}'"
 
@@ -484,14 +495,14 @@ class Triangle(object):
 
         otherType = type(other)
         if issubclass(otherType, Triangle):
-            for s in self.sides:
-                for q in other.sides:
+            for s in self.sides.values():
+                for q in other.sides.values():
                     if s.doesIntersect(q):
                         return True
             return False
 
         if issubclass(otherType, Line):
-            for s in self.sides:
+            for s in self.sides.values():
                 if s.doesIntersect(other):
                     return True
             return False
