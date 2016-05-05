fa25078ac865c7988a2060341b3907fdadda3089
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2016-05-05 10:11:41 -0500

    minor edit to ExceededEpsilonError message

--- Geometry/exceptions.py
+++ Geometry/exceptions.py
@@ -4,35 +4,48 @@
 
 
 class ZeroSlope(Exception):
+    '''
+    '''
     pass
 
-
 class InfiniteSlope(Exception):
+    '''
+    '''
     pass
 
 
 class CollinearPoints(Exception):
+    '''
+    '''
     pass
 
 
 class CollinearLines(CollinearPoints):
+    '''
+    '''
     pass
 
 
 class ParallelLines(Exception):
+    '''
+    '''
     pass
 
 
 class InfiniteLength(Exception):
+    '''
+    '''
     pass
 
 
 class ExceededEpsilonError(Exception):
-
+    '''
+    '''
     def __init(self, x, y, epsilon):
         self.x, self.y, self.epsilon = x, y, epsilon
 
     def __str__(self):
-        fmt = 'x={x} - y={y} {delta} > epsilon {epsilon}'
-        return fmt.format(x=self.x, y=self.y,
-                          delta=self.x - self.y, epsilon=self.epsilon)
+        fmt = 'x={} - y={} {} > epsilon {}'
+        return fmt.format(self.x, self.y,
+                          self.x - self.y,
+                          self.epsilon)
